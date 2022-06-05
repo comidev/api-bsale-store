d74e3046753bc894fa6ec2db2ad97c2ce30812e4
Omar Miranda <comidev.contacto@gmail.com>
2022-06-05 17:43:01 -0500

    agregando cors origin

--- src/main/java/comidev/apibsale/App.java
+++ src/main/java/comidev/apibsale/App.java
@@ -18,14 +18,8 @@ public class App {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
+				registry.addMapping("/**").allowedOrigins("https://bsalestore.vercel.app");
 			}
 		};
 	}
 }
-
-/*
- * http://localhost:8080/products/sort?by=name&order=asc&numPage=6&limit=10
- *
- *
- */
